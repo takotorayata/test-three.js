df1c222959cccd25c41c17aa6d6a4cfa6fe467ce
Tata <tatarusetskaya@gmail.com>
2021-11-30 15:33:45 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 100;
